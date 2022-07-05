5c0a80bea8d029776569cc9376c26a5d92a421cb
BeatrizCanuto <BeatrizCanuto>
2022-07-05 15:10:32 -0300

    correção de erros

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -18,28 +18,24 @@ export class EntrarComponent implements OnInit {
     private router: Router
   ) { }
 
-  ngOnInit(){
-    window.scroll(0,0)
+  ngOnInit() {
+    window.scroll(0, 0)
   }
-entrar(){
-this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=>{
-  this.usuarioLogin =resp
+  entrar() {
+    this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin) => {
+      this.usuarioLogin = resp
 
-  environment.token = this.usuarioLogin.token,
-  environment.nome = this.usuarioLogin.nome,
-  environment.foto = this.usuarioLogin.foto,
-  environment.id = this.usuarioLogin.id,
+      environment.token = this.usuarioLogin.token,
+        environment.nome = this.usuarioLogin.nome,
+        environment.foto = this.usuarioLogin.foto,
+        environment.id = this.usuarioLogin.id,
 
-console.log(environment.token)
-console.log(environment.nome)
-console.log(environment.foto)
-console.log(environment.id)
 
-  this.router.navigate(['/inicio'])
-}, erro =>{
-  if(erro.status == 500){
-  alert('Usuário ou senha estão incorretos!')
+        this.router.navigate(['/inicio'])
+    }, erro => {
+      if (erro.status == 500) {
+        alert('Usuário ou senha estão incorretos!')
+      }
+    });
   }
-})
-}
 }
