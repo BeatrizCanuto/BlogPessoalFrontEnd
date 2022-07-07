2286290c66724ef2fe337adc88f0c4771df144ab
BeatrizCanuto <BeatrizCanuto>
2022-07-06 23:54:51 -0300

    inicio e tema

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EnvironmentInjector, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -13,14 +14,19 @@ export class AuthService {
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
     return this.http.post<UsuarioLogin>(
       'https://projblogpessoal.herokuapp.com/usuarios/logar',
-      usuarioLogin
-    );
+      usuarioLogin);
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(
       'https://projblogpessoal.herokuapp.com/usuarios/cadastrar',
-      usuario
-    );
+      usuario);
+  }
+  logado(){
+    let ok: boolean = false 
+    if (environment.token != ''){
+      ok = true
+    }
+    return ok
   }
 }
